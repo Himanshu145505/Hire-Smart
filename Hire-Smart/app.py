85af582841011ceb2547a27d095786080fe816c6
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-08-10 19:31:56 +0530

    Update app.py

--- Hire-Smart/app.py
+++ Hire-Smart/app.py
@@ -291,6 +291,7 @@ job_roles = {
 def extract_text_from_pdf(pdf_path):
     text = ""
     try:
+        # pdf path
         pdf_document = fitz.open(pdf_path)
         for page_num in range(len(pdf_document)):
             page = pdf_document.load_page(page_num)
